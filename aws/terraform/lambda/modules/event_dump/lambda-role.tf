7a694bdeae4b067f2983ccc481ac831e2cf15140
nisda <na.ishida.0115@gmail.com>
2024-01-20 02:17:43 +0900

    Delete unnecessary code

--- aws/terraform/lambda/modules/event_dump/lambda-role.tf
+++ aws/terraform/lambda/modules/event_dump/lambda-role.tf
@@ -24,25 +24,3 @@ resource "aws_iam_role_policy_attachment" "lamnda_common_role_basic_policy" {
   role       = aws_iam_role.event_dump_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
-# // for GetParameterStore
-# resource "aws_iam_role_policy" "lamnda_common_role_ssm_ps_policy" {
-#   role = aws_iam_role.event_dump_lambda_role.name
-#   name = "ssm-parameter-store-read-policy"
-#   policy = jsonencode(
-#     {
-#       "Version" : "2012-10-17",
-#       "Statement" : [
-#         {
-#           "Sid" : ""
-#           "Action" : [
-#             "ssm:GetParameter",
-#             "kms:Decrypt",
-#           ],
-#           "Effect" : "Allow",
-#           "Resource" : "*",
-#         }
-#       ]
-#     }
-#   )
-# }
